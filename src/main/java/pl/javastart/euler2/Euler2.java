03ca9a7949bcc22925ad2ab19f5fa595298e856f
usernameMaciej <maciej.przystarz@protonmail.com>
2023-02-24 12:00:32 +0100

    rozwiazanie zadania

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -2,7 +2,32 @@ package pl.javastart.euler2;
 
 public class Euler2 {
 
-    public static void main(String[] args) {
+    private static int[] fibonacciCache;
+    private static int nSumFibonacciNumber;
+    private static final int maxValue = 4_000_000;
 
+    public static void main(String[] args) {
+        int n = 44;
+
+        fibonacciCache = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (fibonacciSequence(i) % 2 == 0) {
+                if (nSumFibonacciNumber < maxValue) {
+                    System.out.print(fibonacciSequence(i) + " ");
+                }
+            }
+        }
+    }
+
+    static public int fibonacciSequence(int n) {
+        if (n <= 1) {
+            return n + 1;
+        }
+
+        nSumFibonacciNumber = (fibonacciSequence(n - 1) + fibonacciSequence(n - 2));
+        fibonacciCache[n] = nSumFibonacciNumber;
+
+        return nSumFibonacciNumber;
     }
 }
